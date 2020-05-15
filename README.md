3fe003eaca027a7d2cef435f3b4be1328a28a366
Lilyxu54 <xll220823@163.com>
2020-05-15 10:26:01 +0800

    报告页面基础已完成2

--- README.md
+++ README.md
@@ -1,11 +1,29 @@
-<!--
- * @Author: your name
- * @Date: 2020-05-15 10:22:40
- * @LastEditTime: 2020-05-15 10:23:20
- * @LastEditors: your name
- * @Description: In User Settings Edit
- * @FilePath: \vue-admin-reportc:\Users\LuoJ\Desktop\report-projects\README.md
- -->
-# report-projects
+# vue-admin-report
 
-测试提交
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+
+我电脑装了http-serve所以直接进入dist文件，启动http-serve
+
+
+安装全局命令，这个命令启动静态文件服务器
+ npm i -g serve
+ 查看
+serve --version
+启动
+serve -s dist
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <el-row :gutter="21">
+        <el-col :span="4" style="background: #0E90FE !important;">
+          <div class="grid-content bg-purple title">瑞蒙特后台管理系统</div>
+        </el-col>
+        <el-col :span="20" style="background: #FFFFFF;">
+          <div class="grid-content bg-purple"></div>
+        </el-col>
+      </el-row>
+    </div>
+    <div class="leftnav">
+      <el-row :gutter="25">
+        <el-col :span="4" class="leftnavleft">
+          <div class="grid-content bg-purple">
+            <el-row class="tac">
+              <el-menu
+                default-active="1"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose"
+                background-color="#545c64"
+                text-color="#fff"
+                active-text-color="#ffd04b"
+              >
+                <router-link to="/">
+                  <el-menu-item index="1">
+                    <i class="el-icon-menu"></i>
+                    <span slot="title">报告管理</span>
+                  </el-menu-item>
+                </router-link>
+                <router-link to="/about">
+                  <el-menu-item index="2">
+                    <i class="el-icon-document"></i>
+                    <span slot="title">空气质量报送管理</span>
+                  </el-menu-item>
+                </router-link>
+                <router-link to="/about2">
+                  <el-menu-item index="3">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">工作指导信息管理</span>
+                  </el-menu-item>
+                </router-link>
+              </el-menu>
+            </el-row>
+          </div>
+        </el-col>
+        <el-col :span="20" style="height: 100%;">
+          <div class="grid-content bg-purple" style>
+            <router-view />
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- <div id="nav">
+      
+    </div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+
+<style lang="less">
+a {
+  text-decoration: none;
+}
+.el-row {
+  margin-right: 0px !important;
+  margin-left: 0px !important;
+}
+.el-col-4 {
+  padding-left: 0px !important;
+  padding-right: 0px !important;
+}
+.el-row:last-child,
+.el-col-4,
+.grid-content,
+.el-col-15 {
+  height: 100%;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background: #f1f3f4;
+}
+
+.el-menu-item * {
+  color: #ffffff;
+}
+
+.header .title {
+  color: #fff;
+  text-align: center;
+  line-height: 35px;
+  font-size: 16px;
+  font-family: Microsoft YaHei;
+  font-weight: bold;
+}
+
+.leftnav {
+  height: 90%;
+}
+
+.tac,
+.el-menu {
+  height: 100%;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+</style>
